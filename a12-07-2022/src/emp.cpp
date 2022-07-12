42c7e47615215f302b994f13aaacbc15464d3168
Kartik0609 <jodhpurkartik@gmail.com>
2022-07-12 16:42:05 +0530

    added assign 12-07-2022

--- a12-07-2022/src/emp.cpp
+++ a12-07-2022/src/emp.cpp
@@ -1,41 +1,46 @@
-
-
-
-
-
 #include"emp.h"
 
 using namespace std;
+
+
 
 int main()
 
 {
 	fstream f1;
-	emp L(456354,"Kartik Sharma","Analyst");
-	int abc;
-	abc == fork();
 
-	if (abc == 0)
-	{
-		cout<<"First child "<<endl;
-		fstream f1;
-		f1.open("emp.txt", ios::app);
-		f1.write((char*)&L, sizeof(L));
-		f1.close();
+	emp E(457437,"Kartik Sharma","Manager");
+
+	int x;
+	x = fork();	
+	
+	if(x == 0)
+	{ 
+		 cout<<"First child starts"<<endl;
+	
+             
+		 fstream f1;
+		  f1.open("emp.txt", ios::app);
+		  f1.write((char*)&E,sizeof(E));
+		  f1.close();
+		 cout<<"First child terminates"<<endl;
 	}
 	else
 	{
-		wait(0);
-		cout<<"First Parent "<<endl;
-		fstream f2;
-		f2.open("emp.txt",ios::in);
+	          wait(0);
+        	cout<<"First parent starts"<<endl;
+	
+	
+		ifstream f2;
+		f2.open("Employee.txt",ios::in);
 		f2.seekg(0);
-		f2.read((char*)&L, sizeof(L));
-		cout<<"emp id "<<L.id;
-		cout<<"emp name "<<L.name;
-		cout<<"designation "<<L.desig;
-
-		f1.close();
+	         f2.read((char*)&E,sizeof(E));
+		 cout<<"employee name: "<<E.name<<endl;
+		 cout<<"designation: "<<E.desig<<endl;
+		 cout<<"emp id: "<<E.id<<endl;
+		  f2.close();
+		 
+		   cout<<"First parent terminates"<<endl;
 	}
 	return 0;
 }
